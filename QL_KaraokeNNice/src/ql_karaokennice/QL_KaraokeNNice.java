0851f1340ebdd4f441fb67def71e9471f8067bf3
MSI\PC BAO THONG <baothong15082003@gmail.com>
2023-10-26 01:44:11 +0700

    Vai cai giao dien

---
+++ QL_KaraokeNNice/src/ql_karaokennice/QL_KaraokeNNice.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ql_karaokennice;
+
+import gui.DSPhong;
+import gui.GD_DichVu;
+import gui.GD_NhanVien;
+import gui.HomePage;
+import gui.LoginForm;
+
+/**
+ *
+ * @author PC BAO THONG
+ */
+public class QL_KaraokeNNice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //new LoginForm().setVisible(true);
+//        new HomePage().setVisible(true);
+        //new DSPhong().setVisible(true);
+        new GD_NhanVien().setVisible(true);
+        //new GD_DichVu().setVisible(true);
+    }
+
+}
