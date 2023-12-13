5bcbf06707b3e215ec32276100ac987534a49192
MSI\PC BAO THONG <baothong15082003@gmail.com>
2023-12-13 22:47:39 +0700

    Final

--- QL_KaraokeNNice/src/ql_karaokennice/QL_KaraokeNNice.java
+++ QL_KaraokeNNice/src/ql_karaokennice/QL_KaraokeNNice.java
@@ -16,7 +16,7 @@ public class QL_KaraokeNNice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //new splashscreen.SplashScreen(null, true).setVisible(true);
+        new splashscreen.SplashScreen(null, true).setVisible(true);
         new LoginForm().setVisible(true);
     }
 }
